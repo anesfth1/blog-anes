a0940b2962ccb963d9f59f586740ad29059226f5
anesfth1 <anesfth24>
2019-11-25 11:45:22 +0700

    Belajar Template

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+  <!-- Head -->
+  <?php include('../layouts/includes/head.php'); ?>
+  <!-- End Head -->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+  <!-- Header -->
+  <?php include('../layouts/includes/navbar.php'); ?>
+  <!-- End Header -->
+    <div class="app-body">
+    <!-- Sidebar -->
+    <?php include('../layouts/includes/sidebar.php'); ?>
+    <!-- End Sidebar -->
+      <main class="main">Hello World
+      </main>
+    </div>
+  <!-- Footer -->
+<?php include('../layouts/includes/footer.php'); ?>
+  <!-- End Footer -->
+    <!-- CoreUI and necessary plugins-->
+    <!-- Scripts-->
+    <?php include('../layouts/includes/scripts.php'); ?>
+    <!-- End Scripts -->
+
+  </body>
+</html>
---
+++ layouts/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="app-footer">
+      <div>
+        <a href="#">Blog</a>
+        <span>&copy; 2019 SMK ASSALAAM</span>
+      </div>
+      <div class="ml-auto">
+        <span>Powered by</span>
+        <a href="#">SMK ASSALAAM</a>
+      </div>
+    </footer>
